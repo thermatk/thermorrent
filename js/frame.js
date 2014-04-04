ecfa1a0533d04ca9b087e546de386b2a2ac169cd
thermatk <thermatk@thermatk.com>
2014-04-04 14:25:45 +0400

    code for the navbar

---
+++ js/frame.js
@@ -0,0 +1,10 @@
+var gui = require('nw.gui');
+var win = gui.Window.get();
+
+$('#frame-btn-min').on('click', function () {
+    win.minimize();
+});
+
+$('#frame-btn-close').on('click', function () {
+    win.close();
+});
